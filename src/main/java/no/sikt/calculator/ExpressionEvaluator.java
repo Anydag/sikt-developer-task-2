72503715e17e23b3b1b3d27d570aeb8a1688d6fb
Maria Demchenko <maria.demchenko@spv.no>
2022-05-16 08:36:12 +0200

    work in progress

--- src/main/java/no/sikt/calculator/ExpressionEvaluator.java
+++ src/main/java/no/sikt/calculator/ExpressionEvaluator.java
@@ -1,13 +1,72 @@
 package no.sikt.calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static java.util.Objects.nonNull;
 
 public class ExpressionEvaluator {
-    public ExpressionEvaluator(String expression) {
+
+    private static final String UNKNOWN_OPERATION_ERROR = "Supported operations are +\\-*/";
+    private static final String ILLEGAL_DIVIDER_ERROR = "Division by zero is not supported";
+    private static final String UNKNOWN_EXPRESSION_FORMAT = "Provided syntax is not valid";
+
+    private static final Pattern OPERATION_PATTERN = Pattern.compile("(\\d+) (\\d+) ([+\\-*/])");
+    private static final Pattern VALIDATION_PATTERN = Pattern.compile(
+        "^|(\\d+) (\\d+) ([+\\-*/])"
+            + "|(\\d+) (\\d+) ([+\\-*/]) (\\d+) ([+\\-*/])"
+            + "|(\\d+) (\\d+) (\\d+) ([+\\-*/]) ([+\\-*/])"
+            + "|(\\d+) (\\d+) (\\d+) ([+\\-*/]) (\\d+) ([+\\-*/]) ([+\\-*/])|$");
+
+    public ExpressionEvaluator() {}
+
+    public static String evaluate(String expression) {
         assert nonNull(expression);
+
+        Matcher validationMatcher = VALIDATION_PATTERN.matcher(expression.trim());
+        if (!validationMatcher.matches())
+            // TODO: create custom ValidationException
+            throw new IllegalArgumentException(UNKNOWN_EXPRESSION_FORMAT);
+
+        StringBuilder parsed = new StringBuilder();
+        Matcher operationMatcher = OPERATION_PATTERN.matcher(expression);
+
+        if (!operationMatcher.find()) {
+            throw new IllegalArgumentException(UNKNOWN_EXPRESSION_FORMAT);
+        }
+
+        String evaluatedFragment = calculateArithmeticOperation(operationMatcher);
+        // found fragment is identical to original
+        if (expression.equalsIgnoreCase(operationMatcher.group()))
+            return evaluatedFragment;
+
+        // replace found fragment with evaluated
+        parsed.append(expression, 0, operationMatcher.start())
+                .append(evaluatedFragment)
+                .append(expression, operationMatcher.end(), expression.length());
+
+        // process recursively
+        return evaluate(parsed.toString());
     }
 
-    public Integer getResult() {
-        return 2;
+    private static String calculateArithmeticOperation(Matcher twoOperandMatcher) {
+        int first = Integer.parseInt(twoOperandMatcher.group(1));
+        int second = Integer.parseInt(twoOperandMatcher.group(2));
+        // TODO: declare enum
+        switch (twoOperandMatcher.group(3)) {
+            case "+":
+                return String.valueOf(first + second);
+            case "-":
+                return String.valueOf(first - second);
+            case "*":
+                return String.valueOf(first * second);
+            case "/":
+                if (second == 0)
+                    throw new IllegalArgumentException(ILLEGAL_DIVIDER_ERROR);
+                return  String.valueOf(first / second);
+            default: {
+                throw new IllegalArgumentException(UNKNOWN_OPERATION_ERROR);
+            }
+        }
     }
 }
--- src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
+++ src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
@@ -3,14 +3,48 @@ package no.sikt.calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ExpressionEvaluatorTest {
+    // TODO: read resources file to get expected values
 
     @Test
-    void shouldReturnValueWhenExpressionIsValid()  {
-        var expected = 2;
-        var expression = "1 1 +";
-        var actual = new ExpressionEvaluator(expression).getResult();
+    void shouldValidateAndEvaluateTwoOperands() {
+        var expected = "3";
+        var expression = "6 2 /";
+
+        var actual = ExpressionEvaluator.evaluate(expression);
+        assertNotNull(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldValidateAndEvaluateThreeOperands() {
+        var expected = "4";
+        var expression = "7 1 + 2 /";
+
+        var actual = ExpressionEvaluator.evaluate(expression);
+        assertNotNull(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldValidateAndEvaluateThreeInARowOperands() {
+        var expected = "-1";
+        var expression = "6 5 2 + -";
+
+        var actual = ExpressionEvaluator.evaluate(expression);
+        assertNotNull(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldValidateAndEvaluateFourOperands() {
+        var expected = "-9";
+        var expression = "2 6 2 + 3 + -";
+
+        var actual = ExpressionEvaluator.evaluate(expression);
+        assertNotNull(actual);
         assertEquals(expected, actual);
     }
 }
